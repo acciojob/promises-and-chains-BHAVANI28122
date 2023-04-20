10bfbc203a6deb9955c2e5074da4317b2ff11667
BHAVANI28122 <108610488+BHAVANI28122@users.noreply.github.com>
2023-04-20 08:56:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let name = document.getElementById("name");
+let age = document.getElementById("age");
+document.getElementById("btn").addEventListener("submit",(e)=>{
+	e.preventDefault();
+	name = name.value;
+	age = age.value;
+}).then
